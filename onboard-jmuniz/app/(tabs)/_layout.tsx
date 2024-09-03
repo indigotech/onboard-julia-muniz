c1ae9b9ce440c2ec7b4d49df1371255b15eb2155
Taqtile <juliamuniz.cm@usp.br>
2024-09-03 10:28:09 -0300

    clean up

--- onboard-jmuniz/app/(tabs)/_layout.tsx
+++ onboard-jmuniz/app/(tabs)/_layout.tsx
@@ -1,12 +1,12 @@
 import { StyledView } from '@/components/StyledView';
 import { StyledText } from '@/components/StyledText';
 import React from 'react';
-
 export default function TabLayout() {
 
   return (
     <StyledView>
       <StyledText> Hello World </StyledText>
     </StyledView>
+
   );
 }
