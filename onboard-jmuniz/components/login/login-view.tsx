42364f8ae0bba4ae720626a1e83c29a4596b7d9d
Taqtile <juliamuniz.cm@usp.br>
2024-09-04 10:31:08 -0300

    rebase fix login-view

--- onboard-jmuniz/components/login/login-view.tsx
+++ onboard-jmuniz/components/login/login-view.tsx
@@ -4,17 +4,52 @@ import { LoginTextInput } from "./login-text-input";
 import { LoginViewContainer } from "./login-view-container";
 import { LoginTextBox } from "./login-text-box";
 import { LoginLabelText } from "./login-label-text";
+import { useState } from "react";
 
 export function LoginView() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function isProfileValid(): string[] {
+    const result: string[] = [];
+    if (!email) {
+      result.push("E-mail field must not be empty.\n");
+    } else if (!/[\S]*[@][\S]*[.com]/gm.test(email)) {
+      result.push("Must input valid email format: xxxx@xxxx.com.\n");
+    }
+    if (!password) {
+      result.push("Password field must not be empty.\n");
+    } else if (!/^(?=.*[A-Za-z])(?=.*\d)(?=\S{7,}).*/gm.test(password)) {
+      result.push(
+        "Password must have at least 7 characters, 1 letter and 1 digit.\n",
+      );
+    }
+    return result;
+  }
+
+  function evaluateLogIn() {
+    const profileEval: string[] = isProfileValid();
+    if (profileEval.length) {
+      let message = "";
+      for (const item in profileEval) {
+        message += profileEval[item];
+      }
+      alert(message);
+    }
+  }
+
   return (
     <LoginViewContainer>
       <LoginTextBox>
         <LoginLabelText>E-mail</LoginLabelText>
-        <LoginTextInput />
+        <LoginTextInput value={email} onChangeText={setEmail} />
         <LoginLabelText>Password</LoginLabelText>
-        <LoginTextInput />
+        <LoginTextInput value={password} onChangeText={setPassword} />
       </LoginTextBox>
-      <LoginSubmitButton title={"Submit"}></LoginSubmitButton>
+      <LoginSubmitButton
+        title={"Submit"}
+        onPress={evaluateLogIn}
+      ></LoginSubmitButton>
     </LoginViewContainer>
   );
 }
