06612035e16d5bddd6ffe3b5eefc63d2c3631e76
Taqtile <juliamuniz.cm@usp.br>
2024-09-10 18:12:35 -0300

    remove unused component

--- onboard-jmuniz/components/users-list/users-list-loading.tsx
+++
@@ -1,9 +0,0 @@
-import { ActivityIndicator } from "react-native";
-import { InputLabelText } from "../login/input-validation/input-label-text";
-
-export default function UsersListLoading(props: { isLoading: boolean }) {
-  if (props.isLoading) {
-    return <ActivityIndicator size={"small"} />;
-  }
-  return <InputLabelText>No more users found!</InputLabelText>;
-}
