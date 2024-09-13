28133eddc447008a37a52d15133393dba5ef8d78
Taqtile <juliamuniz.cm@usp.br>
2024-09-13 13:01:29 -0300

    Component Lib tweaks

--- onboard-jmuniz/components/common/styled-touchable.tsx
+++ onboard-jmuniz/components/common/styled-touchable.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components/native";
 
 export const StyledTouchable = styled.TouchableHighlight`
-  height: 60px;
+  height: 50px;
   background-color: lightseagreen;
   width: 50%;
   margin: auto;
--- onboard-jmuniz/components/nav/nav-fab/fab-touchable.tsx
+++ onboard-jmuniz/components/nav/nav-fab/fab-touchable.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components/native";
 
 export const FabTouchable = styled(StyledTouchable)`
   width: 60px;
+  height: 60px;
   position: absolute;
   bottom: 20px;
   right: 10%;
